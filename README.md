4135709f483ed58ea9d502f47251cae274795767
John carmichael <johncarmichael1984@gmail.com>
2018-02-08 23:24:25 +0100

    Examples and readme expansion

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # swagger-chunk
-Build swagger with chunks and write to disc.
 
-The output will automatically pickup the version number from the swagger file, or the package.json or a number manually passed in.
+## Summary
+Build and maintain large swagger API documentation files from small manageable chunks.
 
-Currently this must be installed locally, a global package is on the way.
+## How it works
+Using a combination of [json-refs](https://www.npmjs.com/package/json-refs) and [js-yaml](https://www.npmjs.com/package/js-yaml), [swagger-chunk](https://www.npmjs.com/package/swagger-chunk) combines multiple [YAML](http://yaml.org) files to output a single JSON or YAML file. 
+
+If you are familiar with [Swagger](https://swagger.io) then you will likely be familiar with the concept of definitions and referencing them with `$ref` for example:
+ ```
+ $ref: "#/definitions/Weather"
+ ```
+
+Enclosing the ref in quotes prefixing with a # tells swagger to search for the reference within the same document. Using [swagger-chunk](https://www.npmjs.com/package/swagger-chunk) you can use the `$ref` with a value not in quotes, you are able to reference other files. This technique allows you to break up what would otherwise be potentially 1000's of lines into smaller re-usable chunks.
+
+
 
 Call `node node_modules/swagger-chunk/merge.js --help` file with for how to use it.
 
--- merge.js
+++ merge.js
@@ -1,20 +1,21 @@
 const program = require('commander')
-const fs = require('fs')
+const fs      = require('fs')
 const resolve = require('json-refs').resolveRefs
-const YAML = require('js-yaml')
+const YAML    = require('js-yaml')
 
 const logErrorExit = (e) => {
   console.error(e)
   process.exit()
 }
-const readJson = (file) => {
+const readJson     = (file) => {
   try {
     return JSON.parse(fs.readFileSync(file))
-  } catch (err) {
+  }
+  catch (err) {
     return null
   }
 }
-const packageJson = readJson('./package.json')
+const packageJson  = readJson('./package.json')
 
 program
   .version('1.0.0', '-v, --version',)
@@ -28,7 +29,8 @@ program
 
 if (!program.input) {
   logErrorExit('No input provided')
-} else {
+}
+else {
   if (!fs.existsSync(program.input)) {
     logErrorExit('File does not exist. (' + program.input + ')')
   }
@@ -46,55 +48,59 @@ if (!program.extension) {
   program.extension = program.output_format
 }
 
-let root = YAML.safeLoad(fs.readFileSync(program.input).toString());
+let root    = YAML.safeLoad(fs.readFileSync(program.input).toString())
 let options = {
-  filter        : ['relative', 'remote'],
-  loaderOptions : {
-    processContent : function (res, callback) {
+  filter       : ['relative', 'remote'],
+  loaderOptions: {
+    processContent: function (res, callback) {
       try {
         let yml = YAML.safeLoad(res.text)
-        callback(null, YAML.safeLoad(res.text));
-      } catch (e) {
+        callback(null, YAML.safeLoad(res.text))
+      }
+      catch (e) {
         logErrorExit({
           msg: 'Error parsing yml',
-          e:e
+          e  : e
         })
       }
     }
   }
-};
+}
 
 resolve(root, options).then(function (results) {
 
   let resultObject = JSON.stringify(results.resolved, null, 2)
-  let swagVersion = false
+  let swagVersion  = false
 
   let parsedResltObj = JSON.parse(resultObject)
-  if( parsedResltObj.info.version ){
+  if (parsedResltObj.info.version) {
     swagVersion = parsedResltObj.info.version
-  } else if (!program.Version) {
+  }
+  else if (!program.Version) {
     if (packageJson.version) {
       swagVersion = packageJson.version
-    } else {
+    }
+    else {
       // try and get the version from the yml file
       logErrorExit('No version provided and no version in the package.json')
     }
   }
 
-  if (program.output_format === 'yaml' ){
+  if (program.output_format === 'yaml') {
     resultObject = YAML.safeDump(results.resolved)
   }
 
   let outputFilename = program.destination + program.destination_name + '_' + swagVersion + '.' + program.extension
 
-  fs.writeFile(outputFilename, resultObject, function(err) {
-    if(err) {
+  fs.writeFile(outputFilename, resultObject, function (err) {
+    if (err) {
       logErrorExit({
-        error: 'Error writing the to the output destination',
+        error : 'Error writing the to the output destination',
         detail: err
       })
-    } else {
-      console.log("The file was saved!");
     }
-  });
-});
+    else {
+      console.log('The file was saved!')
+    }
+  })
+})
