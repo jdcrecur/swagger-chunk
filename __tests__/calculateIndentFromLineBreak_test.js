846427509cd28a9c56ff3f543a0a5fbab29671c3
John Carmichael <John.Carmichael@partner.bmwgroup.com>
2019-04-02 12:41:44 +1000

    the indentation tests

--- __tests__/calculateIndentFromLineBreak_test.js
+++ __tests__/calculateIndentFromLineBreak_test.js
@@ -7,25 +7,32 @@ describe('calculateIndentFromLineBreak', () => {
     expect(pointer).toEqual(4)
   })
 
-//   it('check 2 space indent from start of file', () => {
-//     let str = `  Weathers: type`
-//     let pointer = calculateIndentFromLineBreak(str, 12)
-//     expect(pointer).toEqual(2)
-//   })
-//
-//   it('check 0 space indent from start of file', () => {
-//     let str = `Weathers: type`
-//     let pointer = calculateIndentFromLineBreak(str, 12)
-//     expect(pointer).toEqual(0)
-//   })
-//
-//   it('check 0 space indent from start of new line', () => {
-//     let str = `
-// Weathers: type`
-//     let pointer = calculateIndentFromLineBreak(str, 12)
-//     expect(pointer).toEqual(0)
-//   })
-//
+  it('check 2 space indent from start of file', () => {
+    let str = `  Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 12)
+    expect(pointer).toEqual(2)
+  })
+
+  it('check 0 space indent from start of file', () => {
+    let str = `Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 12)
+    expect(pointer).toEqual(0)
+  })
+
+  it('check 0 space indent from start of new line', () => {
+    let str = `
+Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 12)
+    expect(pointer).toEqual(0)
+  })
+
+  it('check 2 space indent from start after new line', () => {
+    let str = `
+  Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 12)
+    expect(pointer).toEqual(2)
+  })
+
   it('check 0 space indent not from start of new line', () => {
     let str = `
 Weathers: type
@@ -34,12 +41,22 @@ Weathers: type`
     expect(pointer).toEqual(0)
   })
 
+  it('check 0 space indent not from start of new line but after a new line', () => {
+    let str = `
+
+Weathers: type
+Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 27)
+    expect(pointer).toEqual(0)
+  })
+
   it('check 2 space indent from start of new line', () => {
     let str = `
-  Weathers: type`
+   Weathers: type`
     let pointer = calculateIndentFromLineBreak(str, 12)
     expect(pointer).toEqual(2)
   })
+
   it('check 2 space indent from not start of new line', () => {
     let str = `
     Weathers: type
@@ -47,11 +64,28 @@ Weathers: type`
     let pointer = calculateIndentFromLineBreak(str, 33)
     expect(pointer).toEqual(2)
   })
+
   it('check 4 space indent from not start of new line', () => {
     let str = `
     Weathers: type
     Weathers: type`
     let pointer = calculateIndentFromLineBreak(str, 35)
     expect(pointer).toEqual(4)
+  })
+  it('check 4 space indent from not start of new line', () => {
+    let str = `
+    Weathers: type
+    Weathers: type
+    Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 54)
+    expect(pointer).toEqual(4)
+  })
+  it('check 4 space indent from not start of new line', () => {
+    let str = `
+    Weathers: type
+    Weathers: type
+      Weathers: type`
+    let pointer = calculateIndentFromLineBreak(str, 56)
+    expect(pointer).toEqual(6)
   })
 })
--- es6/calculateIndentFromLineBreak.js
+++ es6/calculateIndentFromLineBreak.js
@@ -8,21 +8,22 @@ export default (str, pointer) => {
   let spaceStart = false
   let spaceStartPointer = 0
   let spaceEndPointer = 0
-  let spaceCorrection = 0
   for (let i = pointer; i >= 0; --i) {
     if (str[i] === ' ' && !spaceStart) {
       spaceStart = true
       spaceStartPointer = i
-    } else if (str[i] === "\n" || i === 0) {
+    } else if (str[i] === '\n' || i === 0) {
       spaceEndPointer = i
-      if (i === 0 && spaceStartPointer !== 0) {
-        // ++spaceCorrection
+      if (str[i] === '\n' && i === 0 && spaceStartPointer > 2) {
+        ++spaceEndPointer
+      } else if (str[i] !== '\n' && i === 0 && spaceStartPointer > 0) {
+        --spaceEndPointer
       }
       break
-    } else if(str[i] !== ' ') {
+    } else if (str[i] !== ' ') {
       spaceStart = false
       spaceStartPointer = 0
     }
   }
-  return (spaceStartPointer === 0) ? 0 : spaceStartPointer - spaceEndPointer + spaceCorrection
+  return (spaceStartPointer === 0) ? 0 : spaceStartPointer - spaceEndPointer
 }
